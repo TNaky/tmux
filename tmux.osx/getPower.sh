b221348b560df1f0e2f20370515b1f9223c4709c
michibata <michibata@Cecilia.flabRouter>
2016-12-30 17:09:32 +0900

    バッテリー未搭載のノートPC以外における電源状態を非表示化

--- tmux.osx/getPower.sh
+++ tmux.osx/getPower.sh
@@ -3,25 +3,25 @@
 BATTERY="'Battery Power'"
 ACPOWER="'AC Power'"
 # 電池残量を取得
-POWER=`/usr/bin/pmset -g ps`
-POWER_TYPE=`echo ${POWER} | head -n 1 | awk "match(\\$0,/\\047.*\\047/) {print substr(\\$0,RSTART,RLENGTH)}"`
+POWER_STAT=`/usr/bin/pmset -g ps`
+POWER_TYPE=`echo ${POWER_STAT} | head -n 1 | awk "match(\\$0,/\\047.*\\047/) {print substr(\\$0,RSTART,RLENGTH)}"`
 # 電池残量を取得
-POWER=`echo ${POWER} | head -n 1 | awk "match(\\$0,/[0-9]+\\045/) {print substr(\\$0,RSTART, RLENGTH)}" | sed -e 's/\([^0-9]\)//g'`
+POWER_LVL=`echo ${POWER_STAT} | head -n 1 | awk "match(\\$0,/[0-9]+\\045/) {print substr(\\$0,RSTART, RLENGTH)}" | sed -e 's/\([^0-9]\)//g'`
 
 if [ "${POWER_TYPE}" = "${BATTERY}" ]; then
   # 電池残量によって色を設定
   COLOUR=""
-  if [ "$POWER" -gt 60 ]; then
+  if [ "$POWER_LVL" -gt 60 ]; then
     # 緑
     COLOUR="#[fg=colour46]"
-  elif [ "$POWER" -gt 30 ]; then
+  elif [ "$POWER_LVL" -gt 30 ]; then
     # 黄
     COLOUR="#[fg=colour226]"
   else
     # 赤
     COLOUR="#[fg=colour1]"
   fi
-  echo "🔋 ${COLOUR}${POWER}%"
-else
-  echo "⚡ ${COLOUR}${POWER}%"
+  echo "🔋 ${COLOUR}${POWER_LVL}%"
+elif [ `expr $(echo "${POWER_STAT}" | wc -l)` -eq 2 ]; then
+  echo "⚡ ${COLOUR}${POWER_LVL}%"
 fi
